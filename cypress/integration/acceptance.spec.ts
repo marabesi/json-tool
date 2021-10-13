ea2869196cc9e29c7f046b2da072a500551b5621
marabesi <matheus.marabesi@gmail.com>
2021-10-13 15:23:44 +0200

    wip: paste from clipboard button
    acceptance test using clipboard
    
    refs https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/testing-dom__clipboard/cypress/integration/spec.js

--- cypress/integration/acceptance.spec.ts
+++ cypress/integration/acceptance.spec.ts
@@ -13,4 +13,18 @@ describe('json utility tool', () => {
     cy.get('[data-testid="result"]').should('have.value', 'this is not a json');
     cy.get('[data-testid="error"]').should('have.text', 'invalid json');
   });
+
+  it('should place text from clipboard in the editor on click button', () => {
+    cy.visit(url);
+    const desiredJson = '{"name":"json from clipboard"}';
+
+    cy.window()
+      .its('navigator.clipboard')
+      .invoke('writeText', desiredJson);
+
+    cy.get('[data-testid="paste-from-clipboard"]').click();
+
+    cy.get('[data-testid="json"]').should('have.value', desiredJson);
+    cy.get('[data-testid="result"]').should('have.value', desiredJson);
+  });
 });
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,13 +1,4 @@
-const clipboardy = require('clipboardy')
 module.exports = (
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions,
-) => {
-  on('task', {
-    // Clipboard test plugin
-    getClipboard: () => {
-      const clipboard: string = clipboardy.readSync()
-      return clipboard
-    },
-  })
-}
+) => {}
