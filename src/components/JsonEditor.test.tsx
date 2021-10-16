4be83bca9e986013ad82e1954bb3e28be683a846
marabesi <matheus.marabesi@gmail.com>
2021-10-16 11:55:20 +0200

    chore: removed unused test file

--- src/components/JsonEditor.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../App';
-
-describe('json editor', () => {
-  test('place code from int json editor', () => {
-    render(<App />);
-  });
-});
