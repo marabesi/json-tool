5ec52286fce04e7c9462c5706b65da49725a2508
marabesi <matheus.marabesi@gmail.com>
2021-11-29 23:16:45 +0100

    refactor: replaces raw string by constant

--- src/core/cleanUp.ts
+++ src/core/cleanUp.ts
@@ -3,6 +3,7 @@ export default class CleanUp {
   private IS_INSIDE_JSON_VALUE: number = 3;
   private ENDED_JSON_PAIR_KEY_VALUE: number = 4;
   private STARTED_INSIDE_JSON_KEY: number = 1;
+  private DOUBLE_QUOTES: string = '"';
 
   cleanWhiteSpaces(originalJson: string): string {
     return this.cleanUpJsonFrom(' ', originalJson);
@@ -19,7 +20,7 @@ export default class CleanUp {
     for (let i = 0; i < originalJson.length; i++) {
       const current = originalJson[i];
 
-      if (current === '"') {
+      if (current === this.DOUBLE_QUOTES) {
         stack.push(current);
       }
 
