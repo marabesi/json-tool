fd7eaeb0d6efecf965e22b5019f29abbfde1ee3e
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-05-07 20:42:35 +0200

    chore(input): custom class name

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,13 +1,16 @@
 type InputTextProps = {
   onChange?: (value: string) => void;
   value?: string
+  className?: string
 }
 
-export default function inputText({ value, onChange, ...rest }: InputTextProps) {
+export default function inputText({ value, onChange, className, ...rest }: InputTextProps) {
+  const classes = [className, ''].join(' ').trim();
   return (
     <input
       type="text"
       value={value}
+      className={classes}
       onChange={eventValue => onChange ? onChange(eventValue.target.value) : null}
       {...rest}
     />
