0f0bd56781d83d9cbfedda612c84a9d9b7a6dcac
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-07-24 23:10:07 +0200

    chore: adds contenteditable to editor

--- cypress/support/step_definitions/RemoveSpacesAndNewLines.js
+++ cypress/support/step_definitions/RemoveSpacesAndNewLines.js
@@ -1,25 +1,25 @@
-const { When, Then } = require("cypress-cucumber-preprocessor/steps");
+const { When, Then } = require('cypress-cucumber-preprocessor/steps');
 
 When(/^I place a json string with new lines and white spaces$/, function () {
-this.jsonWithNewLinesAndSpaces = `{
+  this.jsonWithNewLinesAndSpaces = `{
   "test" : "test",
   "name" : "json from clipboard"
 }`;
 
-    cy.get('[data-testid="json"]').type(this.jsonWithNewLinesAndSpaces, { parseSpecialCharSequences: true });
+  cy.get('[data-testid="json"] .cm-content').type(this.jsonWithNewLinesAndSpaces, { parseSpecialCharSequences: true });
 });
 
 When(/^I click to clean white spaces and new lines$/, function () {
-    cy.get('[data-testid="clean-new-lines-and-spaces"]').click();
+  cy.get('[data-testid="clean-new-lines-and-spaces"]').click();
 });
 
 Then(/^I see the json with new lines and white spaces in the left editor$/, function () {
-    // cy.get('[data-testid="json"]').should('have.value', this.jsonWithNewLinesAndSpaces);
-    return 'pending';
+  // cy.get('[data-testid="json"]').should('have.value', this.jsonWithNewLinesAndSpaces);
+  return 'pending';
 });
 
 Then(/^I see the json without new lines and white spaces in the right editor$/, function () {
-    cy.get('[data-testid="result"] .cm-content').should(($div) => {
-        expect($div.get(0).innerText).to.eq('{"test":"test","name":"json from clipboard"}');
-    });
+  cy.get('[data-testid="result"] .cm-content').should(($div) => {
+    expect($div.get(0).innerText).to.eq('{"test":"test","name":"json from clipboard"}');
+  });
 });
