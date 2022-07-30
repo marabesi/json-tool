9ba94ef35a25b875e7891cb737ce3ff510506515
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-07-30 19:40:45 +0200

    fix: attempt to avoid error when using type from cypress

--- cypress/support/step_definitions/FormatJsonSteps.js
+++ cypress/support/step_definitions/FormatJsonSteps.js
@@ -1,28 +1,28 @@
-const { When, Then } = require("cypress-cucumber-preprocessor/steps");
+const { When, Then } = require('cypress-cucumber-preprocessor/steps');
 
 When(/^I open json tool with valid json string$/, function () {
-    cy.visit('/');
-    cy.get('[data-testid="json"]').type('{}');
+  cy.visit('/');
+  cy.get('[data-testid="json"] .cm-content').type('{}');
 });
 
 Then(/^I see the same json in the right editor$/, function () {
-    cy.get('[data-testid="result"] .cm-content').should(
-        ($div) => {
-            expect($div.get(0).innerText).to.eq('{}');
-        }
-    );
+  cy.get('[data-testid="result"] .cm-content').should(
+    ($div) => {
+      expect($div.get(0).innerText).to.eq('{}');
+    }
+  );
 });
 
 When(/^I open json tool with an invalid json string$/, function () {
-    cy.visit('/');
-    cy.get('[data-testid="json"]').type('this is not a json');
-    cy.get('[data-testid="result"] .cm-content').should(
-        ($div) => {
-            expect($div.get(0).innerText).to.eq('this is not a json');
-        }
-    );
+  cy.visit('/');
+  cy.get('[data-testid="json"]').type('this is not a json');
+  cy.get('[data-testid="result"] .cm-content').should(
+    ($div) => {
+      expect($div.get(0).innerText).to.eq('this is not a json');
+    }
+  );
 });
 
 Then(/^I see an error message$/, function () {
-    cy.get('[data-testid="error"]').should('have.text', 'invalid json');
+  cy.get('[data-testid="error"]').should('have.text', 'invalid json');
 });
