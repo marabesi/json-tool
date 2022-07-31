21e6eec3fc239f2246bbdf0cc3ac85c2a51f7deb
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-07-31 14:28:49 +0200

    fix: eslint identation

--- src/components/ui/JsonEditor.tsx
+++ src/components/ui/JsonEditor.tsx
@@ -28,31 +28,32 @@ export default function JsonEditor({ input, onChange, className, ...rest }: Prop
 
   return (
     <SettingsContext.Consumer>
-      {settings => {
-        const basicSetup: BasicSetupOptions = {};
-        settings.forEach((item : Option) => basicSetup[item.title as keyof BasicSetupOptions] = item.active);
+      {
+        settings => {
+          const basicSetup: BasicSetupOptions = {};
+          settings.forEach((item : Option) => basicSetup[item.title as keyof BasicSetupOptions] = item.active);
 
-        return (
-          <>
-            <textarea data-testid={`raw-${rest['data-testid']}`} className="hidden" defaultValue={input}></textarea>
-            <CodeMirror
-              value={input}
-              onChange={handleChange}
-              className={[className, 'h-full'].join(' ')}
-              style={{
-                fontSize: 12,
-                backgroundColor: fullConfig.theme.backgroundColor.gray['200'],
-                overflowY: 'hidden',
-                fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-              }}
-              height="100%"
-              extensions={[json()]}
-              basicSetup={basicSetup}
-              {...rest}
-            />
-          </>
-        );
-      }
+          return (
+            <>
+              <textarea data-testid={`raw-${rest['data-testid']}`} className="hidden" defaultValue={input}></textarea>
+              <CodeMirror
+                value={input}
+                onChange={handleChange}
+                className={[className, 'h-full'].join(' ')}
+                style={{
+                  fontSize: 12,
+                  backgroundColor: fullConfig.theme.backgroundColor.gray['200'],
+                  overflowY: 'hidden',
+                  fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                }}
+                height="100%"
+                extensions={[json()]}
+                basicSetup={basicSetup}
+                {...rest}
+              />
+            </>
+          );
+        }
       }
     </SettingsContext.Consumer>
   );
