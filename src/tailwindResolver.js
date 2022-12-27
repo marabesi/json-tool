b719b6dc6b8d6513c55d97400c3b95984102e685
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-12-27 10:49:10 +0100

    chore: instrument code with cypress module

--- src/tailwindResolver.js
+++ src/tailwindResolver.js
@@ -1,3 +1,7 @@
+/* istanbul ignore file */
+
+// ignore file is in here because of cypress instrumentation code
+
 // @preval
 const resolveConfig = require('tailwindcss/resolveConfig');
 const tailwindConfig = require('../tailwind.config');
