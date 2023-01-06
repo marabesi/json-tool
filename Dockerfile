1a12a0df73aac3f1d083f2398253fdc6cbac7a57
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-01-06 11:36:06 +0100

    Revert "chore: updated dependencies"
    This reverts commit 65a5f32bd43e8ea905109ab73117d30b271e68ee.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM node:18
+FROM node:16
 WORKDIR /app
 COPY package.json ./
 COPY package-lock.json ./
 COPY ./ ./
-RUN npm i --legacy-peer-deps
+RUN npm i
 CMD ["npm", "run", "start"]
--- before-push.sh
+++ before-push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 docker-compose run --rm json-tool rm -rf node_modules/
-docker-compose run --rm json-tool npm i --legacy-peer-deps
+docker-compose run --rm json-tool npm i
 docker-compose run --rm json-tool npm run build
 docker-compose run --rm -d json-tool npm run start
 echo "Sleeping 10 seconds for the application to start"
 sleep 10
 echo "Woke up, after 10 seconds, running tests now..."
-docker-compose run --rm -e CI=true json-tool npm run test -- --silent
-docker run --rm -v "$(pwd)":/app -w /app cypress/included:12.1.0 npm run e2e
+docker-compose run --rm -e CI=true json-tool npm run test
+docker run --rm -v $(pwd):/app -w /app cypress/included:3.2.0 npm run e2e
 docker-compose down --remove-orphans
