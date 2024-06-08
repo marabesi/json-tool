f71703711f9b3f9f2ca375ee0689c601b84874a7
marabesi <matheus.marabesi@gmail.com>
2024-06-08 21:11:39 +0200

    fix: test case linter rules
    refs https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-render-in-lifecycle.md

--- src/__test__/CleanUpEditors.test.tsx
+++ src/__test__/CleanUpEditors.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, RenderResult, waitFor } from '@testing-library/react';
+import { act, render, RenderResult, waitFor, screen } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 import { grabCurrentEditor } from './__testutilities__/editorQuery';
@@ -6,20 +6,18 @@ import { setUpClipboard, tearDownClipboard, writeTextToClipboard } from 'jest-cl
 import { customType } from './__testutilities__/customTyping';
 
 describe('Clean up editors', () => {
-  let wrapper: RenderResult;
 
   beforeEach(() => {
     setUpClipboard();
-    wrapper = render(<App />);
   });
 
   afterEach(() => {
     tearDownClipboard();
-    wrapper.unmount();
   });
 
   it('should clean editors once clean is clicked', async () => {
-    const { container, getByTestId } = wrapper;
+    const view: RenderResult = render(<App />);
+    const { container } = view;
 
     const editor = grabCurrentEditor(container);
 
@@ -27,14 +25,12 @@ describe('Clean up editors', () => {
       await customType(editor, '{{}');
     });
 
-    await act(async () => {
-      await userEvent.click(getByTestId('clean'));
-    });
+    await userEvent.click(screen.getByTestId('clean'));
 
     await waitFor(() => {
-      expect(getByTestId('raw-json')).toHaveValue('');
-      expect(getByTestId('raw-result')).toHaveValue('');
+      expect(screen.getByTestId('raw-json')).toHaveValue('');
     });
+    expect(screen.getByTestId('raw-result')).toHaveValue('');
   });
 
   it.each([
@@ -43,24 +39,20 @@ describe('Clean up editors', () => {
   "last_name" : "another name"
 }`, '{"name":"json from clipboard","last_name":"another name"}'],
   ])('should clean blank spaces and new lines in the json (%s, %s)', async (inputJson: string, desiredJson: string) => {
-    const { getByTestId } = wrapper;
+    render(<App />);
 
     await writeTextToClipboard(inputJson);
 
-    await act(async () => {
-      await userEvent.click(getByTestId('paste-from-clipboard'));
-    });
+    await userEvent.click(screen.getByTestId('paste-from-clipboard'));
 
     await waitFor(() => {
-      expect(getByTestId('raw-json')).toHaveValue(inputJson);
+      expect(screen.getByTestId('raw-json')).toHaveValue(inputJson);
     });
 
-    await act(async () => {
-      await userEvent.click(getByTestId('clean-new-lines-and-spaces'));
-    });
+    await userEvent.click(screen.getByTestId('clean-new-lines-and-spaces'));
 
     await waitFor(() => {
-      expect(getByTestId('raw-result')).toHaveValue(desiredJson);
+      expect(screen.getByTestId('raw-result')).toHaveValue(desiredJson);
     });
   });
 });
