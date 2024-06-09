b7c4aff0f417afe16e4616823ddf340289f853cf
marabesi <matheus.marabesi@gmail.com>
2024-06-09 12:15:03 +0200

    fix: linter issues

--- src/__test__/components/InputText.test.tsx
+++ src/__test__/components/InputText.test.tsx
@@ -5,12 +5,12 @@ describe('input text component', () => {
 
   test('renders input text', () => {
     render(<InputText data-testid="text"/>);
-    expect(screen.queryByTestId('text')).toBeInTheDocument();
+    expect(screen.getByTestId('text')).toBeInTheDocument();
   });
 
   test('renders input text with custom class', () => {
     const className = 'randomclass';
     render(<InputText data-testid="text" className={className}/>);
-    expect(screen.queryByTestId('text')).toHaveAttribute('class', expect.stringContaining(className));
+    expect(screen.getByTestId('text')).toHaveAttribute('class', expect.stringContaining(className));
   });
 });
