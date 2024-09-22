c08517eaa5d33df4cefcd75cedbf7e7cf1244466
marabesi <matheus.marabesi@gmail.com>
2024-09-22 17:04:21 +0200

    chore: upgrade to electron 32

--- before-push.sh
+++ before-push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-docker-compose run --rm json-tool rm -rf node_modules/ coverage/ build/
-docker-compose run --rm json-tool npm i --legacy-peer-deps
-docker-compose run --rm json-tool npm run build
-docker-compose run --service-ports --rm -d json-tool npm run start
+docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/
+docker compose run --rm json-tool npm i --legacy-peer-deps
+docker compose run --rm json-tool npm run build
+docker compose run --service-ports --rm -d json-tool npm run start
 echo "Sleeping 10 seconds for the application to start"
 sleep 10
 echo "Woke up, after 10 seconds, running tests now..."
-docker-compose run --rm -e CI=true json-tool npm run test
+docker compose run --rm -e CI=true json-tool npm run test
 docker run --network=host --rm -e CYPRESS_BASE_URL=http://localhost:3000 -v $(pwd):/app -w /app cypress/included:12.1.0 npm run e2e
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
+
+docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/
