bf6b78afee9f536a97bb20a0c75f5a821c7c7bb3
marabesi <matheus.marabesi@gmail.com>
2024-09-22 21:52:54 +0200

    chore: upgrade cypress coverage to 3.13.1

--- before-push.sh
+++ before-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/
+docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/ .nyc_output
 docker compose run --rm json-tool npm i --legacy-peer-deps
 docker compose run --rm json-tool npm run build
 docker compose run --service-ports --rm -d json-tool npm run start
@@ -10,5 +10,3 @@ echo "Woke up, after 10 seconds, running tests now..."
 docker compose run --rm -e CI=true json-tool npm run test
 docker run --network=host --rm -e CYPRESS_BASE_URL=http://localhost:3000 -v $(pwd):/app -w /app cypress/included:13.14.1 npm run e2e
 docker compose down --remove-orphans
-
-docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/
