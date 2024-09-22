fb3d066df02583e7954ec2b7091e95e309e78516
marabesi <matheus.marabesi@gmail.com>
2024-09-22 18:11:37 +0200

    chore: upgraded to cypress for before-push.sh

--- before-push.sh
+++ before-push.sh
@@ -8,7 +8,7 @@ echo "Sleeping 10 seconds for the application to start"
 sleep 10
 echo "Woke up, after 10 seconds, running tests now..."
 docker compose run --rm -e CI=true json-tool npm run test
-docker run --network=host --rm -e CYPRESS_BASE_URL=http://localhost:3000 -v $(pwd):/app -w /app cypress/included:12.1.0 npm run e2e
+docker run --network=host --rm -e CYPRESS_BASE_URL=http://localhost:3000 -v $(pwd):/app -w /app cypress/included:13.14.1 npm run e2e
 docker compose down --remove-orphans
 
 docker compose run --rm json-tool rm -rf node_modules/ coverage/ build/
