c0fbb02e5589b01f7eee10c00681c3325a0838f4
marabesi <matheus.marabesi@gmail.com>
2024-10-10 21:28:30 +0200

    docs: updated

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,12 +1,25 @@
 export function Docs() {
   return (
     <div className="p-10">
-      <h1 className="text-xl pb-10">JSON tool docs</h1>
-      <ul>
-        <li>
+      <h1 className="text-2xl pb-10">JSON tool docs</h1>
+      <ul className="pt-5">
+        <li className="pb-5">
           <a href="https://marabesi.com/tools/json-tool-a-companion-for-formatting-json-strings.html?utm_source=json-tool&utm_medium=direct&utm_campaign=jsontool&utm_id=json-tool-docs" target="_blank" rel="noreferrer">
             Why JSON tool?
           </a>
+        </li>
+        <li>
+          <details>
+            <summary>
+              <span className="text-xl mt-5">The technology behind JSON tool</span>
+
+            </summary>
+          
+            <ul className="pt-1">
+              <li>
+                <a href="https://marabesi.com/javascript/web-workers-to-the-rescue-how-to-work-with-json-strings-without-blocking-user-interactions.html?utm_source=json-tool&utm_medium=direct&utm_campaign=jsontool&utm_id=json-tool-docs" target="_blank" rel="noreferrer">Web workers</a></li>
+            </ul>
+          </details>
         </li>
       </ul>
     </div>
