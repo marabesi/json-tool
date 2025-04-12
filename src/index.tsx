b7c396ead23d821994dc2ae0d533543a2a9a0472
marabesi <matheus.marabesi@gmail.com>
2025-04-12 12:43:06 -0300

    chore: removed webvitals package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 
 import './index.scss';
@@ -13,7 +12,3 @@ root.render(
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
