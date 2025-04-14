4c8d955a4dc113dfdf167a4c5346506effd9b320
marabesi <matheus.marabesi@gmail.com>
2025-04-14 18:57:35 -0300

    chore: clean up dependencies

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,9 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-// import 'jsdom-worker';
+import 'jsdom-worker';
 import { TextEncoder, TextDecoder } from 'util';
-import './jsdom';
 
 const fmt2json = require('format-to-json');
 
