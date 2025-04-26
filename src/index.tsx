64a31a1a195e33c404426e7131ea6227b3176b40
marabesi <matheus.marabesi@gmail.com>
2025-04-26 14:27:23 -0300

    chore: using css instead of scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
-import './index.scss';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
