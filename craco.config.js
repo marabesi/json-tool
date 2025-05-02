258c4649b31c4857986b23c12b088139f85626ff
marabesi <matheus.marabesi@gmail.com>
2025-05-02 12:00:26 -0300

    chore: removed postcss dep

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  style: {
-    postcss: {
-      plugins: (plugins) => [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ].concat(plugins),
-    },
-  },
   babel: {
     plugins: ['preval']
   }
