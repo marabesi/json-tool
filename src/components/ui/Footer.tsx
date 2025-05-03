1233573f8c8af0c407fdeeadf5c62d03b830bf4f
marabesi <matheus.marabesi@gmail.com>
2025-05-03 16:09:02 -0300

    chore: moving ui elements around

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaBug } from 'react-icons/fa';
+import { FaBug, FaCoffee } from 'react-icons/fa';
 
 export default function Footer({ ...props }) {
   return (
@@ -6,15 +6,22 @@ export default function Footer({ ...props }) {
       <a href="https://github.com/marabesi/json-tool/blob/main/LICENSE.md" target="_blank" rel="noreferrer">
         CC0 1.0 Universal
       </a>
-      <a
-        data-testid="found-issue"
-        href="https://github.com/marabesi/json-tool/issues"
-        target="_blank"
-        rel="noreferrer"
-        className="flex items-center"
-      >
-        <FaBug className="mr-2" /> Found an issue?
-      </a>
+      <div className="flex">
+        <a
+          data-testid="found-issue"
+          href="https://github.com/marabesi/json-tool/issues"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mr-5"
+        >
+          <FaBug className="mr-2" /> Found an issue?
+        </a>
+
+        <a className="flex items-center" data-testid="buy-me-a-coffee" href="https://www.buymeacoffee.com/marabesi" target="_blank" rel="noreferrer">
+          <FaCoffee className="mr-2" />
+          <span>Buy me a coffee</span>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,4 +1,5 @@
-import { FaCoffee, FaRegSun, FaRegLightbulb, FaHistory } from 'react-icons/fa';
+import { ReactNode } from 'react';
+import { FaRegSun, FaRegLightbulb, FaHistory } from 'react-icons/fa';
 import { Link } from 'react-router';
 import Switch from 'react-switch';
 import fullConfig from '../../tailwindResolver';
@@ -7,6 +8,14 @@ import { useThemeContext } from '../../DarkModeContext';
 import { useSettingsContext } from '../../settings/SettingsContext';
 import { useDrawerContext } from '../../DrawerContext';
 
+function TabWrapper({ children }: { children: ReactNode }) {
+  return (
+    <div className="p-5 hover:bg-blue-400 hover:dark:bg-gray-600">
+      {children}
+    </div>
+  );
+}
+
 export default function Header() {
   const { isValidateEnabled, setValidateEnabled } = usePersistenceContext();
   const { onDarkThemeChanged, darkModeEnabled  } = useThemeContext();
@@ -14,28 +23,46 @@ export default function Header() {
   const{ toggle } = useDrawerContext();
 
   return (
-    <div className="bg-blue-900 flex justify-between p-5 dark:bg-gray-700">
-      <div className="flex items-center">
-        <Link to="/" data-testid="to-home" className="flex">
-          <h2 className="text-yellow-400 font-bold">
-          JSON tool
-          </h2>|by marabesi
-        </Link>
-        <div className="ml-10">
-          <input type="checkbox" id="is-validate-json" onChange={() => setValidateEnabled(!isValidateEnabled)}
-            data-testid="is-validate-json" checked={isValidateEnabled} className="mr-1" />
-          <label htmlFor="is-validate-json">validate json</label>
-        </div>
+    <div className="bg-blue-900 flex justify-between dark:bg-gray-700">
+      <div className="h-14 flex items-center">
+        <TabWrapper>
+          <Link to="/" data-testid="to-home" className="flex">
+            <h2 className="text-yellow-400 font-bold">
+              JSON tool
+            </h2>|by marabesi
+          </Link>
+        </TabWrapper>
+        <TabWrapper>
+          <Link data-testid="docs" to="/docs" title="JSON tool documentation">
+            <FaRegLightbulb />
+          </Link>
+        </TabWrapper>
+        <TabWrapper>
+          <Link data-testid="settings" to="/settings" title="JSON tool settings">
+            <FaRegSun />
+          </Link>
+        </TabWrapper>
       </div>
+
       <div className="flex items-center">
         {isHistoryEnabled &&
           <button data-testid="json-drawer-history-button" className="mr-5" onClick={toggle}>
             <FaHistory />
           </button>
         }
-        <Link className="flex items-center mr-5" data-testid="docs" to="/docs" title="JSON tool documentation">
-          <FaRegLightbulb />
-        </Link>
+        
+        <div className="mr-5">
+          <input
+            type="checkbox"
+            id="is-validate-json"
+            onChange={() => setValidateEnabled(!isValidateEnabled)}
+            data-testid="is-validate-json"
+            checked={isValidateEnabled}
+            className="mr-1 cursor-pointer"
+          />
+          <label htmlFor="is-validate-json" className="cursor-pointer">validate json</label>
+        </div>
+        
         <Switch
           onChange={onDarkThemeChanged}
           checked={darkModeEnabled}
@@ -48,13 +75,6 @@ export default function Header() {
           checkedIcon={false}
           uncheckedIcon={false}
         />
-        <Link className="flex items-center mr-5" data-testid="settings" to="/settings" title="JSON tool settings">
-          <FaRegSun />
-        </Link>
-        <a className="flex items-center" data-testid="buy-me-a-coffee" href="https://www.buymeacoffee.com/marabesi" target="_blank" rel="noreferrer">
-          <FaCoffee className="mr-2" />
-          <span>Buy me a coffee</span>
-        </a>
       </div>
     </div>
   );
