5600ae9d7b50debbd74311a84ed25639cb5d0051
marabesi <2129872+marabesi@users.noreply.github.com>
2026-03-01 19:46:03 +0100

    fix: cypress run from before-push

--- cypress.config.js
+++ cypress.config.js
@@ -16,16 +16,14 @@ module.exports = defineConfig({
     ],
     'experimentalRunAllSpecs': true,
     setupNodeEvents(on, config) {
-      if (process.env.ELECTRON_EXTRA_LAUNCH_ARGS) {
-        on('task', {
-          prepareArchives: () => {
-            // Your task logic here (e.g., file operations, cleanup, etc.)
-            console.log('Preparing archives...');
-            return null; // or return a value if needed
-          },
-        });
-        installPlugin(on, config);
-      }
+      on('task', {
+        prepareArchives: () => {
+          // Your task logic here (e.g., file operations, cleanup, etc.)
+          console.log('Preparing archives...');
+          return null; // or return a value if needed
+        },
+      });
+      installPlugin(on, config);
       coverage(on, config);
       on('file:preprocessor', cucumber());
       return config;
